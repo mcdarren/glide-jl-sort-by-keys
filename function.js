2b6d914e775caac2a34efba653bc2ac26c7e9c88
Darren Murphy <darren@victoriajd.com>
2021-11-23 18:49:05 +0800

    Add support for lexical/numerical sorting, and asc/desc ordering

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
-window.function = function (data, sort_keys, delimiter) {
+window.function = function (data, sort_keys, delimiter, type, order) {
   if (data.value === undefined) return undefined;
   if (sort_keys.value === undefined) return undefined;
   var delim = delimiter.value ? delimiter.value : ',';
+  var sort_type = type.value ? type.value : 'numerical';
+  var ordering = order.value ? order.value : 'desc';
 
   var data_arr = data.value.split(delim);
   var keys_arr = sort_keys.value.split(delim);
@@ -9,9 +11,17 @@ window.function = function (data, sort_keys, delimiter) {
   for (i=0; i<data_arr.length; i++) {
     obj[data_arr[i]] = keys_arr[i];
   }
-  var sorted = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]});
+  var sorted;
+  if (sort_type === 'lexical') {
+    sorted = Object.keys(obj).sort((a,b) => (obj[b] > obj[a]) ? 1 : ((obj[a] > obj[b]) ? -1 : 0));
+  }
+  else {
+    sorted = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]});
+  }
+  if (ordering === 'asc') {
+    sorted = sorted.reverse();
+  }
   var joined = sorted.join('');
 
   return joined;
-
 }
