dab47c7a40a4bed4630f90b9926b6e730d8c7109
Darren Murphy <darren@victoriajd.com>
2021-11-23 19:08:49 +0800

    Revert "Add support for lexical/numerical sorting, and asc/desc ordering"
    This reverts commit 2b6d914e775caac2a34efba653bc2ac26c7e9c88.

--- function.js
+++ function.js
@@ -1,9 +1,7 @@
-window.function = function (data, sort_keys, delimiter, type, order) {
+window.function = function (data, sort_keys, delimiter) {
   if (data.value === undefined) return undefined;
   if (sort_keys.value === undefined) return undefined;
   var delim = delimiter.value ? delimiter.value : ',';
-  var sort_type = type.value ? type.value : 'numerical';
-  var ordering = order.value ? order.value : 'desc';
 
   var data_arr = data.value.split(delim);
   var keys_arr = sort_keys.value.split(delim);
@@ -11,17 +9,9 @@ window.function = function (data, sort_keys, delimiter, type, order) {
   for (i=0; i<data_arr.length; i++) {
     obj[data_arr[i]] = keys_arr[i];
   }
-  var sorted;
-  if (sort_type === 'lexical') {
-    sorted = Object.keys(obj).sort((a,b) => (obj[b] > obj[a]) ? 1 : ((obj[a] > obj[b]) ? -1 : 0));
-  }
-  else {
-    sorted = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]});
-  }
-  if (ordering === 'asc') {
-    sorted = sorted.reverse();
-  }
+  var sorted = Object.keys(obj).sort(function(a,b){return obj[b]-obj[a]});
   var joined = sorted.join('');
 
   return joined;
+
 }
